44ec9e59e80c9abdfaadb3af26e4aed7abc47730
Vitaly <vytalymedvedev@gmail.com>
2023-12-23 17:03:29 +0600

    Remove commit wrapping

--- controller/machine.controller.js
+++ controller/machine.controller.js
@@ -2,15 +2,10 @@ const pool = require('../database-connection');
 
 class MachineController {
   async createMachine(req, res) {
-    let client;
-
     try {
-      client = await pool.connect();
-      await client.query('BEGIN');
-
       const { machine_size, drink_capacity } = req.body;
 
-      const select = await client.query(
+      const select = await pool.query(
         'SELECT id, amount FROM CoffeeMachine WHERE machine_size = $1 AND drink_capacity = $2',
         [machine_size, drink_capacity]
       );
@@ -19,30 +14,22 @@ class MachineController {
       if(select.rows.length) {
         const { id } = select.rows[0];
 
-        response = await  client.query(
+        response = await  pool.query(
           'UPDATE CoffeeMachine SET amount = amount + 1 WHERE id = $1 RETURNING *',
           [id]
         );
       } else {
-        response = await  client.query(
+        response = await  pool.query(
           'INSERT INTO CoffeeMachine (machine_size, drink_capacity, amount) VALUES ($1, $2, $3) RETURNING *',
           [machine_size, drink_capacity, 1]
         );
       }
-      
-      await client.query('COMMIT');
 
       res.json(response.rows[0]);
     } catch(error) {
-      await client.query('ROLLBACK');
-
       res.status(500).send({
         message: 'Cannot insert or update row'
      });
-    } finally {
-      if(client) {
-        client.release();
-      }
     }
   }
 
