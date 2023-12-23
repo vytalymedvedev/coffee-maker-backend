b8273fd93c2dbb9cd1ed06508f83cd5e1561552e
Vitaly <vytalymedvedev@gmail.com>
2023-12-23 23:35:44 +0600

    Add cors for usage on same machine

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const machineRouter = require('./routes/machine.routes');
+const cors = require("cors");
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
 app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PATCH']
+}));
 app.use('/api', machineRouter);
+
 
 app.listen(PORT);
